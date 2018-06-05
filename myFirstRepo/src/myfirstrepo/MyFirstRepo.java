75b861652a0f73cf42bebd449e305c836170274b
AnnaBit <33482313+bergerlein@users.noreply.github.com>
2018-06-05 10:43:06 +0200

    Update MyFirstRepo.java

--- myFirstRepo/src/myfirstrepo/MyFirstRepo.java
+++ myFirstRepo/src/myfirstrepo/MyFirstRepo.java
@@ -19,6 +19,7 @@ public class MyFirstRepo {
     
 public static void main(String[] args) {
         System.out.println("Das ist mein erstes Repo");
+        System.out.printin("Das ist mein Branch");
         
     }
     
